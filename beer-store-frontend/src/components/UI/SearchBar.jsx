4ecd5653d723946b63ca1cf5c218a7846e3eb183
Bladimir Gerson Parra Bermudez <info@ipproyectosysoluciones.com>
2023-09-30 13:00:02 -0500

    Changes in the Title of the Site. Changes to SearchBar styles

--- beer-store-frontend/src/components/UI/SearchBar.jsx
+++ beer-store-frontend/src/components/UI/SearchBar.jsx
@@ -21,12 +21,14 @@ const SearchBar = () => {
     };
   
  
-    return(
-        <div>
-            <input value={brand} onChange={handleInputChange} type='search' placeholder='Your favorite brand here'/>            
-            <button onClick={handleOnClick}>Search</button>            
-        </div>
-    )
+    return (
+			<div>
+				<input value={brand} onChange={handleInputChange} type='search' placeholder='Your favorite brand here' />
+				<button className='bg-primary px-8 py-2 text-gray-100 hover:bg-secondary' onClick={handleOnClick}>
+					Search
+				</button>
+			</div>
+		);
 };
 
 export default SearchBar;
