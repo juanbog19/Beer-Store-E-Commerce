8bb196db3c91d9329787eaa550fe6c8ccb0c77f8
KariRomero <karinaromero.1993@gmail.com>
2023-10-02 13:40:57 -0300

    About

--- beer-store-frontend/src/components/pages/About.jsx
+++ beer-store-frontend/src/components/pages/About.jsx
@@ -7,46 +7,35 @@ const About = () => {
 
   return (
     <>
-      <h1 className="text-center text-xl mb-4 mt-2">About</h1>
+      <h1 className="text-center text-xl mb-4 mt-2">¡Bienvenidos a nuestra página web de E-Beer-Store!</h1>
       <p>
-        Non et proident magna voluptate. Ullamco eu id Lorem mollit. Veniam labore et
-        ipsum dolore nisi irure amet est dolor ea proident aute laborum. Eiusmod est
-        amet in tempor magna veniam occaecat. Ea ut cupidatat aliqua aute sint
-        commodo sunt exercitation aliquip ut minim aliquip enim velit. Sunt
-        reprehenderit mollit fugiat dolor voluptate dolor. Laboris anim cillum velit
-        esse sunt nisi ea eiusmod eu exercitation proident elit culpa do.
+      En CervezaArte.mx, nos enorgullece presentar un emocionante e-commerce dedicado a la venta de cervezas artesanales de la 
+      más alta calidad. Nuestra plataforma ha sido diseñada para satisfacer los gustos más exigentes de los amantes de la cerveza,
+      brindando una experiencia única de compra y descubrimiento.Nuestros principales objetivos son:
       </p>
       <p>
-        Non et proident magna voluptate. Ullamco eu id Lorem mollit. Veniam labore et
-        ipsum dolore nisi irure amet est dolor ea proident aute laborum. Eiusmod est
-        amet in tempor magna veniam occaecat. Ea ut cupidatat aliqua aute sint
-        commodo sunt exercitation aliquip ut minim aliquip enim velit. Sunt
-        reprehenderit mollit fugiat dolor voluptate dolor. Laboris anim cillum velit
-        esse sunt nisi ea eiusmod eu exercitation proident elit culpa do.
+      E-commerce de Cervezas Artesanales: Somos tu destino en línea para explorar y comprar una amplia variedad de cervezas artesanales. Desde las cervecerías 
+      más reconocidas hasta las joyas ocultas de la producción local, ofrecemos una selección diversa y emocionante para satisfacer todos los paladares.
       </p>
       <p>
-        Non et proident magna voluptate. Ullamco eu id Lorem mollit. Veniam labore et
-        ipsum dolore nisi irure amet est dolor ea proident aute laborum. Eiusmod est
-        amet in tempor magna veniam occaecat. Ea ut cupidatat aliqua aute sint
-        commodo sunt exercitation aliquip ut minim aliquip enim velit. Sunt
-        reprehenderit mollit fugiat dolor voluptate dolor. Laboris anim cillum velit
-        esse sunt nisi ea eiusmod eu exercitation proident elit culpa do.
+      Facilitamos la Venta de Cervezas Artesanales: En CervezaArte.mx, creemos en la diversidad y la inclusión. Damos la bienvenida a todas las marcas de 
+      cerveza artesanal, grandes y pequeñas, para que promocionen y vendan sus productos en nuestra plataforma. Esto crea un espacio donde las cervecerías 
+      emergentes pueden llegar a una audiencia más amplia y diversa.
       </p>
       <p>
-        Non et proident magna voluptate. Ullamco eu id Lorem mollit. Veniam labore et
-        ipsum dolore nisi irure amet est dolor ea proident aute laborum. Eiusmod est
-        amet in tempor magna veniam occaecat. Ea ut cupidatat aliqua aute sint
-        commodo sunt exercitation aliquip ut minim aliquip enim velit. Sunt
-        reprehenderit mollit fugiat dolor voluptate dolor. Laboris anim cillum velit
-        esse sunt nisi ea eiusmod eu exercitation proident elit culpa do.
+      Incentivamos el Desarrollo de Pequeños y Medianos Emprendimientos: Creemos en el poder de la cerveza artesanal para impulsar la creatividad y la 
+      innovación. Nuestra plataforma brinda a los emprendedores de cerveza artesanal la oportunidad de crecer y prosperar al conectarlos con una base de 
+      clientes apasionados.
       </p>
       <p>
-        Non et proident magna voluptate. Ullamco eu id Lorem mollit. Veniam labore et
-        ipsum dolore nisi irure amet est dolor ea proident aute laborum. Eiusmod est
-        amet in tempor magna veniam occaecat. Ea ut cupidatat aliqua aute sint
-        commodo sunt exercitation aliquip ut minim aliquip enim velit. Sunt
-        reprehenderit mollit fugiat dolor voluptate dolor. Laboris anim cillum velit
-        esse sunt nisi ea eiusmod eu exercitation proident elit culpa do.
+      Apoyo a través de Publicidad Relacionada: Para garantizar que nuestra plataforma siga siendo un destino vibrante y emocionante para los amantes de la 
+      cerveza, colaboramos con marcas y productos relacionados que complementan la experiencia de nuestros usuarios. La publicidad en nuestra plataforma no 
+      solo promociona productos de alta calidad, sino que también apoya el crecimiento continuo de la comunidad cervecera artesanal.
+      </p>
+      <p>
+      En E-Beer-Store, estamos comprometidos con la excelencia, la diversidad y el crecimiento de la industria cervecera artesanal. Te invitamos a explorar 
+      nuestra selección, descubrir nuevas cervezas y ser parte de nuestra comunidad cervecera en línea. ¡Gracias por unirte a nosotros en esta emocionante 
+      aventura cervecera!
       </p>
       {/*<Navigate to='/' replace={ true } /> */}
     </>
