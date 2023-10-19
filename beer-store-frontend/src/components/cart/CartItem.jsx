c4eb288a2abbb90c6f139dfbdbbc6d0f8b025975
OmarCarrillo1121 <omarcarrillo1121@gmail.com>
2023-10-19 04:52:42 -0600

    Admin dashboard- Agregado relacion al crear brand- Faltan corregir bugs

--- beer-store-frontend/src/components/cart/CartItem.jsx
+++ beer-store-frontend/src/components/cart/CartItem.jsx
@@ -5,44 +5,44 @@ El componente CartItem es un componente de React que muestra un elemento del car
 import { useDispatch } from "react-redux";
 import { addItem, removeItem } from "../../store/cartSlice";
 
-const CartItem = ( props ) => {
+const CartItem = (props) => {
   const { name, price, quant, id, img } = props;
   const totalItem = price * quant;
   const dispatch = useDispatch();
 
   const add = () => {
-    dispatch( addItem({ ...props, amount:1 }) );
-  }
+    dispatch(addItem({ ...props, amount: 1 }));
+  };
 
   const remove = () => {
-    dispatch( removeItem( id ));
-  }
-  
+    dispatch(removeItem(id));
+  };
+
   return (
     <li className="my-2 pb-2 border-b border-secondary flex justify-between">
       <div className="flex">
-        <img src={ img.url } alt={ name } className={`w-16 h-16 rounded-full shadow-lg mr-2`} />
+        {img ? (
+          <img
+            src={img.url}
+            alt={name}
+            className={`w-16 h-16 rounded-full shadow-lg mr-2`}
+          />
+        ) : (
+          <p>Missing img</p>
+        )}
 
         <div>
-          <h3 
-            className="text-xl font-bold"
-          >
-            { name }
-          </h3>
-          <div 
-            className="text-stone-600 text-sm"
-          >
-            Unit: ${ price }
-          </div>
-          <button 
-            className="font-semibold text-sm text-stone-900 hover:text-primary" 
-            onClick={ remove }
+          <h3 className="text-xl font-bold">{name}</h3>
+          <div className="text-stone-600 text-sm">Unit: ${price}</div>
+          <button
+            className="font-semibold text-sm text-stone-900 hover:text-primary"
+            onClick={remove}
           >
             Remove
           </button>
-          <button 
-            className="font-semibold text-sm text-stone-900 hover:text-primary ml-1" 
-            onClick={ add }
+          <button
+            className="font-semibold text-sm text-stone-900 hover:text-primary ml-1"
+            onClick={add}
           >
             Add
           </button>
@@ -50,11 +50,13 @@ const CartItem = ( props ) => {
       </div>
 
       <div className="text-right grid content-end">
-        <span className="uppercase tracking-wide text-gray-700 text-xs font-bold">Amount x ( { quant } )</span>
-        <span className="text-semibold text-lg text-primary">${ totalItem }</span>
+        <span className="uppercase tracking-wide text-gray-700 text-xs font-bold">
+          Amount x ( {quant} )
+        </span>
+        <span className="text-semibold text-lg text-primary">${totalItem}</span>
       </div>
     </li>
-  )
-}
+  );
+};
 
 export default CartItem;
