e86c8694cb06b41eaf0b011f8cefea7a68dde647
OmarCarrillo1121 <omarcarrillo1121@gmail.com>
2023-10-19 00:37:02 -0600

    Admin dashboard- Agregado condicional para denegar acceso a dashboard si no es admin

--- beer-store-frontend/src/store/authSlice.jsx
+++ beer-store-frontend/src/store/authSlice.jsx
@@ -9,31 +9,30 @@ El slice utiliza el método "createAsyncThunk" para manejar las acciones asincr�
 
 El slice exporta la acción "logout" y el reducer por defecto.
 */
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from '../tools/axiosInstance';
-import emailjs from 'emailjs-com';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../tools/axiosInstance";
+import emailjs from "emailjs-com";
 
-emailjs.init('O16QNAbD4CtFz7Hmb');
-const initialState = { loggedin:true, user:null, jwt:null, loading:false };
-
+emailjs.init("O16QNAbD4CtFz7Hmb");
+const initialState = { loggedin: true, user: null, jwt: null, loading: false };
 
 // Test
 // const delay = ( time ) => {
 //   return new Promise( resolve => setTimeout( resolve, time ));
 // }
 
-export const login = createAsyncThunk( 
-  'auth/login',
-  async({ identifier, password }, thunkApi) => {
+export const login = createAsyncThunk(
+  "auth/login",
+  async ({ identifier, password }, thunkApi) => {
     try {
-      const response = await axios.post('api/auth/local/', { 
-        identifier, 
-        password 
+      const response = await axios.post("api/auth/local/", {
+        identifier,
+        password,
       });
       return response;
-    } catch ( error ) {
-      console.log( error );
-      return thunkApi.rejectWithValue( error );
+    } catch (error) {
+      console.log(error);
+      return thunkApi.rejectWithValue(error);
     }
   }
 );
@@ -53,7 +52,6 @@ export const loginWithGoogle = createAsyncThunk(
   }
 );
 
-
 export const userRegister = createAsyncThunk(
   "auth/register",
   async ({ username, email, password }, thunkApi) => {
@@ -67,66 +65,66 @@ export const userRegister = createAsyncThunk(
         to_email: email, // Dirección de correo electrónico del usuario registrado
       };
 
-      await emailjs.send('service_7g4qye6', 'template_kiwb6to', templateParams);
+      await emailjs.send("service_7g4qye6", "template_kiwb6to", templateParams);
       return response;
-    } catch ( error ) {
-      console.log( error );
-      return thunkApi.rejectWithValue( error );
+    } catch (error) {
+      console.log(error);
+      return thunkApi.rejectWithValue(error);
     }
   }
 );
 
 const authSlice = createSlice({
-  name:'auth',
+  name: "auth",
   initialState,
-  reducers:{
-    logout( state ){
-      state.loggedin = false
-      state.user = null
-      state.jwt = null
-    }
+  reducers: {
+    logout(state) {
+      state.loggedin = false;
+      state.user = { isAdmin: false };
+      state.jwt = null;
+    },
   },
-  extraReducers:( builder ) => {
-    builder.addCase( userRegister.pending, ( state, action ) => {
+  extraReducers: (builder) => {
+    builder.addCase(userRegister.pending, (state, action) => {
       state.loading = true;
     });
-    builder.addCase( userRegister.fulfilled, ( state, action ) => {
+    builder.addCase(userRegister.fulfilled, (state, action) => {
       state.loading = false;
       state.loggedin = true;
       state.user = action.payload.data.user;
       state.jwt = action.payload.data.jwt;
     });
-    builder.addCase(userRegister.rejected, ( state, action ) => {
+    builder.addCase(userRegister.rejected, (state, action) => {
       state.loading = false;
     });
     builder.addCase(loginWithGoogle.pending, (state) => {
-      console.log( 'pending', action );
+      console.log("pending", action);
       state.loading = true;
     });
     builder.addCase(loginWithGoogle.fulfilled, (state, action) => {
-      console.log( 'fulfilled', action );
+      console.log("fulfilled", action);
       state.loading = false;
       state.loggedin = true;
       state.user = action.payload.user;
       state.jwt = action.payload.jwt;
     });
     builder.addCase(loginWithGoogle.rejected, (state) => {
-      console.log( 'rejected', action );
+      console.log("rejected", action);
       state.loading = false;
     });
-    builder.addCase( login.pending, ( state, action ) => {
-      console.log( 'pending', action );
+    builder.addCase(login.pending, (state, action) => {
+      console.log("pending", action);
       state.loading = true;
     });
-    builder.addCase( login.fulfilled, ( state, action ) => {
-      console.log( 'fulfilled', action );
+    builder.addCase(login.fulfilled, (state, action) => {
+      console.log("fulfilled", action);
       state.loggedin = true;
       state.user = action.payload.data.user;
       state.jwt = action.payload.data.jwt;
       state.loading = false;
     });
-    builder.addCase( login.rejected, ( state, action ) => {
-      console.log( 'rejected', action );
+    builder.addCase(login.rejected, (state, action) => {
+      console.log("rejected", action);
       state.loading = false;
     });
   },
